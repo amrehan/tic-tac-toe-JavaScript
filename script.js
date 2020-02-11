66db5fee2ac2854a4cca44e236d5e1f771d1f51e
Rehan Alam <48210765+amrehan@users.noreply.github.com>
2020-02-11 20:23:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+var userChoice = prompt('Chose X or O');
+
+if (userChoice === 'x' || userChoice === 'X') {
+  userImage = 'images/cross.png';
+  compImage = 'images/zero.png';
+}
+else if (userChoice === 'o' || userChoice === 'O' || userChoice === '0')
+{
+    userImage = 'images/zero.png';
+    compImage = 'images/cross.png';
+}
+
+else {
+  alert('please enter only X or O');
+  var userChoice = prompt('choose X or O');
+}
+
+$(document).keydown(function() {
+  userPlay(event.key);
+  console.log('yes');
+  compPlay(event.key);
+  gameOver();
+});
+
+
+function userPlay(value) {
+  $('.' + value).attr('src', userImage);
+}
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function compPlay(value, array) {
+  var random = Math.floor(Math.random()*(array.length - 1) + 1);
+  // if (value == 1 && $('.' + 2).attr('src') != userImage){
+  //   random = 2;
+  //   $('.' + random).attr('src', compImage);
+  // }else if (value == 1 && $('.' + 4).attr('src') != userImage)
+  // {
+  //   random = 4;
+  //   $('.' + random).attr('src', compImage);
+  // }
+  // else{
+  //   $('.' + random).attr('src', compImage);}
+
+  array.splice(value-1, 1);
+  $('.' + array[random])
+
+}
+
+$('.1') == userImage && $('.2') == userImage && $('.3') == userImage
+
+function gameOver() {
+  if ($('.1').attr('src') == userImage && $('.2').attr('src') == userImage && $('.3').attr('src') == userImage &&
+  $('.1').attr('src') == $('.2').attr('src') &&
+$('.2').attr('src') == $('.3').attr('src') &&
+$('.1').attr('src') == $('.3').attr('src'))
+{
+    $('h1').text('You Won!!!!')
+}
+else if (
+$('.4').attr('src') == userImage && $('.5').attr('.src') == userImage && $('.6').attr('src') == userImage &&
+  $('.4').attr('src') == $('.5').attr('src') &&
+$('.5').attr('src') == $('.6').attr('src') &&
+$('.6').attr('src') == $('.4').attr('src'))
+{
+  $('h1').text('You Won!!!!')
+}
+else if (
+$('.7').attr('src') == userImage && $('.8').attr('.src') == userImage && $('.9').attr('src') == userImage &&
+  $('.7').attr('src') == $('.8').attr('src') &&
+$('.8').attr('src') == $('.9').attr('src') &&
+$('.7').attr('src') == $('.9').attr('src'))
+{
+  $('h1').text('You Won!!!!')
+}
+else if (
+$('.5').attr('src') == userImage && $('.7').attr('.src') == userImage && $('.3').attr('src') == userImage &&
+  $('.7').attr('src') == $('.5').attr('src') &&
+$('.5').attr('src') == $('.3').attr('src') &&
+$('.7').attr('src') == $('.3').attr('src'))
+{
+  $('h1').text('You Won!!!!')
+}
+else if (
+$('.1').attr('src') == userImage && $('.5').attr('.src') == userImage && $('.9').attr('src') == userImage &&
+  $('.9').attr('src') == $('.5').attr('src') &&
+$('.5').attr('src') == $('.1').attr('src') &&
+$('.1').attr('src') == $('.9').attr('src'))
+{
+  $('h1').text('You Won!!!!')
+}
+}
